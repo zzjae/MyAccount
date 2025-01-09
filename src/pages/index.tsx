ebf978e9c45c30f8cc9e6e3ddfed467d794529cb
GRAM <jgseo200par@naver,com>
2025-01-09 14:26:01 +0900

    ✨ My_asset
    my_asset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,21 @@
 //코드 분리
 import dynamic from 'next/dynamic';
 
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-
-import Skeleton from '@shared/Skeleton';
+import Account from '@home/Account';
+import { BannerSkeleton } from '@home/EventBanners';
 
 const EventBanners = dynamic(() => import('@home/EventBanners'), {
   ssr: false, //서버 사이드 X 클라이언트 사이드 렌더링
   loading: () => (
     //들썩임 방지
-    <Skeleton width="100%" height={100} style={{ borderRadius: 8 }}>
-      로딩중...
-    </Skeleton>
+    <BannerSkeleton />
   ),
 });
 export default function Home() {
   return (
-    <Container>
+    <>
       <EventBanners />
-      <div>Hello</div>
-    </Container>
+      <Account />
+    </>
   );
 }
-
-const Container = styled.div`
-  background-color: pink;
-`;
-
-const bold = css`
-  font-weight: bold;
-`;
